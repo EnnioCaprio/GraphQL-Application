18f84e169a56802ff2425b130ff02e95909977a3
Ennio Caprio <ennio520@gmail.com>
2020-08-17 11:36:44 +0200

    update

--- src/index.js
+++ src/index.js
@@ -35,10 +35,10 @@ mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONG
 
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('frontend/build'));
+	app.use(express.static('client/build'));
 
 	app.get('*', (request, response) => {
-		response.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+		response.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 	});
 }
 
